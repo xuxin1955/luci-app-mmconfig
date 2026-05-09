e7f806a00833d4c26bab9b255e48a030f60be7dd
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-05-10 07:46:02 +0800

    Add files via upload

--- luasrc/model/cbi/modem/modemconfig.lua
+++ luasrc/model/cbi/modem/modemconfig.lua
@@ -1,6 +1,3 @@
--- Copyright 2008 Yanira <forum-2008@email.de>
--- Licensed to the public under the Apache License 2.0.
-
 require("nixio.fs")
 
 local m
@@ -9,76 +6,62 @@ local mode2g = os.execute("/usr/share/modeminfo/scripts/getmode.sh 2g")
 local mode3g = os.execute("/usr/share/modeminfo/scripts/getmode.sh 3g")
 local mode4g = os.execute("/usr/share/modeminfo/scripts/getmode.sh 4g")
 
-m = Map("modemconfig", translate("Configure modem bands"),
-	translate("Configuration 2G/3G/4G modem frequency bands."))
+m = Map("modemconfig", translate("配置调制解调器频段"),
+    translate("配置 2G/3G/4G 调制解调器的频率频段。"))
 
-s = m:section(TypedSection, "modem", "<p>&nbsp;</p>" .. translate("Choose bands cellular modem"))
+s = m:section(TypedSection, "modem", "<p>&nbsp;</p>" .. translate("选择蜂窝调制解调器频段"))
 s.anonymous = true
+s.description = translate("请根据你的 SIM 卡和运营商支持情况选择频段。")
 
--- disable broken
---[[ 
-netmode = s:option(ListValue, "mode", translate("Net Mode"),
-translate("Preffered Network mode select."))
-if mode4g == 0 then
-	netmode:value("4g", "4G only")
-end
-if mode4g == 0 and mode3g == 0 then
-	netmode:value("p4g3g", "4G/3G: preffer 4G")
-	netmode:value("4gp3g", "4G/3G: preffer 3G")
-end
-if mode2g == 0 and mode3g == 0 and mode4g == 0 then
-	netmode:value("p4g3g2g", "4G/3G/2G: preffer 4G")
-	netmode:value("4gp3g2g", "4G/3G/2G: preffer 3G")
-	netmode:value("4g3gp2g", "4G/3G/2G: preffer 2G")
-end
-if mode3g == 0 then
-	netmode:value("3g", "3G only")
-end
-if mode3g == 0 and mode2g == 0 then
-	netmode:value("p3g2g", "3G/2G: preffer 3G")
-	netmode:value("3gp2g", "3G/2G: preffer 2G")
-end
+-- 2G
 if mode2g == 0 then
-	netmode:value("2g", "2G only")
-end
-netmode.default = "p4g3g"
-]]--
-
-if mode2g == 0 then
-	gsm = s:option(StaticList, "gsm_band", translate("2G"))
-	gsm:value("8", "GSM900") 
-	gsm:value("3", "GSM1800")
-	gsm.rmempty = true
+    s:option(DummyValue, "_2g_h", "").description =
+        "<strong>2G</strong> <small>移动/联通：GSM900 / 1800</small>"
+    gsm = s:option(StaticList, "gsm_band", "")
+    gsm:value("8", "GSM900")
+    gsm:value("3", "GSM1800")
+    gsm.rmempty = true
 end
 
+-- 3G
 if mode3g == 0 then
-	wcdma = s:option(StaticList, "3g_band", translate("3G"))
-	wcdma:value("9", "WCDMA850")
-	wcdma:value("8", "WCDMA900")
-	wcdma:value("1", "WCDMA2100")
-	s.rmempty = true
+    s:option(DummyValue, "_3g_h", "").description =
+        "<strong>3G</strong> <small>联通：WCDMA850 / 900 / 2100</small>"
+    wcdma = s:option(StaticList, "3g_band", "")
+    wcdma:value("9", "WCDMA850")
+    wcdma:value("8", "WCDMA900")
+    wcdma:value("1", "WCDMA2100")
+    wcdma.rmempty = true
 end
 
+-- 4G FDD
 if mode4g == 0 then
-	ltefdd = s:option(StaticList, "lte_band_fdd", translate("4G FDD"))
-	ltefdd:value("1", "B1")
-	ltefdd:value("3", "B3")
-	ltefdd:value("5", "B5")
-	ltefdd:value("7", "B7")
-	ltefdd:value("8", "B8")
-	ltefdd:value("20", "B20")
-	s.rmempty = true
+    s:option(DummyValue, "_4g_fdd_h", "").description =
+        "<strong>4G FDD</strong> <small>联通：B1 / B3（主力），B7 / B8；电信：B1 / B3 / B5（主力），B8 / B20</small>"
 
-	ltetdd = s:option(StaticList, "lte_band_tdd", translate("4G TDD"),
-	translate("Maybe must reconnect cellular interface. <br /> If deselect all bands, then used default band modem config."))
-	ltetdd:value("38", "B38")
-	ltetdd:value("40", "B40")
-	ltetdd:value("41", "B41")
-	s.rmempty = true
+    ltefdd = s:option(StaticList, "lte_band_fdd", "")
+    ltefdd:value("1", "B1")
+    ltefdd:value("3", "B3")
+    ltefdd:value("5", "B5")
+    ltefdd:value("7", "B7")
+    ltefdd:value("8", "B8")
+    ltefdd:value("20", "B20")
+    ltefdd.rmempty = true
+
+-- 4G TDD
+    s:option(DummyValue, "_4g_tdd_h", "").description =
+        "<strong>4G TDD</strong> <small>移动主力：B38 / B40 / B41</small>"
+
+    ltetdd = s:option(StaticList, "lte_band_tdd", "",
+        translate("可能需要重新连接蜂窝网络接口。<br />如果取消选中所有频段，则使用调制解调器默认配置。"))
+    ltetdd:value("38", "B38")
+    ltetdd:value("40", "B40")
+    ltetdd:value("41", "B41")
+    ltetdd.rmempty = true
 end
 
 function m.on_after_commit(Map)
-        luci.sys.call("/usr/bin/modemconfig")
+    luci.sys.call("/usr/bin/modemconfig")
 end
 
 return m
